f93b6add6905d02c28c63775cc2dffdac0da1111
Michael Yohannes <miketj2019@gmail.com>
2020-07-23 23:52:10 +0000

    night/day mode, place markers

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -1,31 +1,40 @@
 // Copyright 2019 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 let map;
+let map_style;
+let establishment_markers = [];
+let marker_dict = {};
 
 /* Builds map object with zoom functionality */
 function generateMap() {
+  let time = new Date();
+  map_style = day_map_style;
+  if (time.getHours() >= 18) { //after 6:00 pm
+    map_style = night_map_style;
+  }
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0}, 
     zoom: 15,
+    styles: map_style
   });
-
-  var card = document.getElementById('pac-card');
-  var input = document.getElementById('pac-input');
-  var types = document.getElementById('type-selector');
-  var strictBounds = document.getElementById('strict-bounds-selector');
-  var autocomplete = new google.maps.places.Autocomplete(input);
+  //map.clearOverlays(); //clear markers
+  let card = document.getElementById('pac-card');
+  let input = document.getElementById('pac-input');
+  let types = document.getElementById('type-selector');
+  let strictBounds = document.getElementById('strict-bounds-selector');
+  let autocomplete = new google.maps.places.Autocomplete(input);
   map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
   // Bind the map's bounds (viewport) property to the autocomplete object,
@@ -37,22 +46,21 @@ function generateMap() {
   autocomplete.setFields(
   ['address_components', 'geometry', 'icon', 'name']);
 
-  var infowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
+  let infowindow = new google.maps.InfoWindow();
+  let infowindowContent = document.getElementById('infowindow-content');
   infowindow.setContent(infowindowContent);
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     map: map,
-    anchorPoint: new google.maps.Point(0, -29)
+    anchorPoint: new google.maps.Point(0, -29),
   });
-
   autocomplete.addListener('place_changed', function() {
     infowindow.close();
     marker.setVisible(false);
-    var place = autocomplete.getPlace();
+    let place = autocomplete.getPlace();
     if (!place.geometry) {
       // User entered the name of a Place that was not suggested and
       // pressed the Enter key, or the Place Details request failed
-      window.alert("No details available for input: '" + place.name + "'");
+      window.alert('No details available for input: \'' + place.name + '\'');
       return;
     }
 
@@ -67,7 +75,7 @@ function generateMap() {
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
 
-    var address = '';
+    let address = '';
     
     if (place.address_components) {
       address = [
@@ -89,7 +97,7 @@ function generateMap() {
 
   // Sets a listener on a radio button to change the filter type on Places Autocomplete
   function setupClickListener(id, types) {
-    var radioButton = document.getElementById(id);
+    let radioButton = document.getElementById(id);
     radioButton.addEventListener('click', function() {
       autocomplete.setTypes(types);
     });
@@ -110,28 +118,67 @@ function generateMap() {
 /* Retrieves places based on a passed longitude and latitude */
 function getPlaces(lat, lng){
   map.setCenter({lat: lat, lng: lng}); 
-  console.log(lat + ", " + lng);
+  console.log(lat + ', ' + lng);
   let radius = 1500; //some predetermined constant
   let link = '/getPlaces?lat=' + lat + '&lng=' + lng + '&radius=' + radius; 
+  let infoWindow = new google.maps.InfoWindow();
+
   fetch(link).then(response => response.text()).then((output) => {
-    //console.log(output);
     let places_dict = JSON.parse(output);
-    console.log(places_dict);
-    console.log(places_dict[1]);
-    console.log(places_dict.html_attributions);
-    console.log(places_dict.results);
+    if (places_dict['error'] === undefined) { 
+      console.log(places_dict)
+      console.log(places_dict.results)
+      let places_result = places_dict.results;
+      if (places_result.length > 0) {
+        for (let i = 0; i < places_result.length; i++) {
+          if(places_result[i].business_status !== undefined){//business, not locality
+          let id = places_result[i].name + ';' + places_result[i].geometry.location.lat + ',' + places_result[i].geometry.location.lng;
+          let marker = new google.maps.Marker({
+            position: {lat: places_result[i].geometry.location.lat, lng: places_result[i].geometry.location.lng},
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+            title: places_result[i].name,
+            id: id
+          });
+          marker_dict[id] = {'place_icon': places_result[i].icon, 'place_name': places_result[i].icon,
+          'place-address': places_result[i].vicinity, 'geometry': places_result[i].geometry.location
+          };
+          google.maps.event.addListener(marker, 'click', function() {
+            let place = marker_dict[marker.id];
+
+            console.log(place)
+            /*let infowindowContent = document.getElementById('infowindow-content');
+            infowindowContent.children['place-icon'].src = place.icon;
+            infowindowContent.children['place-name'].textContent = place.name;
+            infowindowContent.children['place-address'].textContent = place.name;
+            infowindowContent.children['location'].textContent = place.geometry;
+            document.getElementById('form-place-name').textContent = place.name;
+            document.getElementById('form-place-address').textContent = place.vicinity;
+            infoWindow.setContent(infowindowContent);
+
+            infoWindow.open(map, marker);*/
+          });
+          establishment_markers.push(marker);
+          }
+        }
+      } else {//Google Places couldn't find places near the user, 
+        console.log('no places found')
+      }      
+    } else {//issue with the call itself
+      console.log('call failed')
+    }
   });
 }
 
 /* Retrieves counties based on a passed longtiude and latitude */
 function getCounties(){
-  console.log("getCounties");
+  console.log('getCounties');
   getPlaces(-33.00, 151.00);
 }
 
 /* Prints geolocation success to console */
 function userLocationSuccess(location){
-  console.log("userLocationSuccess")
+  console.log('userLocationSuccess')
   getPlaces(location.coords.latitude, location.coords.longitude);
 }
 
