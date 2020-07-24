632c00d4751cad7e00902d94e965ba3696e2c42d
Sean <spcasey99@gmail.com>
2020-07-24 22:29:16 +0000

    Add splash page

---
+++ capstone/target/appengine-staging/day_map.js
@@ -0,0 +1,27 @@
+let day_map_style =     [{
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+    {
+      "featureType": "poi",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }
+  ];
