dd45e765d574e21a5355aa51198275020b7ff7bb
Jerry Li <jerryli0273@gmail.com>
2020-08-05 00:45:27 +0000

    save changes

--- capstone/src/main/webapp/login_script.js
+++ capstone/src/main/webapp/login_script.js
@@ -23,10 +23,7 @@ function checkLogin() {
         document.getElementById("sign_out").style.display = "none";
     } else {
         document.getElementById("report").style.display = "block";
-<<<<<<< HEAD
-=======
         document.getElementById("sign_in").style.display = "none";
->>>>>>> b6db0a2282bfd6a745837b5b7bec9b0a8fab90f1
     }
 }
 
